eeb6e946aea4ecdf34e096091862e7fbfa45019d
henryfogacas <henryfogacas@gmail.com>
2023-08-21 16:30:05 -0300

    site organo finalizado

---
+++ src/components/Rodape/Rodape.jsx
@@ -0,0 +1,11 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (
+        <footer className='rodape'>
+            <img src="/imagens/rodape.png" alt="Rodapé do Organo" />
+        </footer>
+    )
+}
+
+export default Rodape
